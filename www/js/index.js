066639a9decff016be75e2c7efe84374871db0e2
Michael Jimenez <michael@e-media-technology.com>
2014-07-09 15:20:04 +0200

    host jQuery and bootstrap locally

--- www/js/index.js
+++ www/js/index.js
@@ -1,26 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // denotes whether we are within a mobile device (otherwise we're in a browser)
     iAmPhoneGap: false,
     // how long should we wait for PhoneGap to say the device is ready.
-    howPatientAreWe: 5000,
+    howPatientAreWe: 3500,
     // id of the 'too_impatient' timeout
     timeoutID: null,
     // id of the 'impatience_remaining' interval reporting.
@@ -50,7 +33,7 @@ var app = {
                 }
                 areWeThereYet.howLongLeft -= 1000; // not so much longer to wait.
 
-                console.log("areWeThereYet: Will give PhoneGap another " + areWeThereYet.howLongLeft + "ms");
+                //console.log("areWeThereYet: Will give PhoneGap another " + areWeThereYet.howLongLeft + "ms");
             }, 1000);
     },
     // deviceready Event Handler
@@ -72,20 +55,9 @@ var app = {
         $("#" + id).remove();
         $("#btn-test").text("Tester");
 
-        //var parentElement = document.getElementById(id);
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
-
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
-
         // clear the "areWeThereYet" reporting.
         window.clearInterval(app.impatienceProgressIntervalID);
 
-        console.log('Received Event: ' + id);
-
-        //if (app.iAmPhoneGap) {
-        //    $.getScript("http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js");
-        //}
+        //console.log('Received Event: ' + id);
     }
 };
